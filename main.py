72ab98321d2090c5ca9dbcfd5d77119fa3ef8a9b
RepleDEV <ragazzochaesa@gmail.com>
2026-04-28 14:31:04 +0700

    commented unused flags

--- main.py
+++ main.py
@@ -79,9 +79,9 @@ def main():
     
     # Setup 
     parser.add_argument("--ddir", help="set data directory for default setup (defaults to ./data/)", default=DEFAULT_DATA_DIRECTORY)
-    parser.add_argument("-p", "--point", help="point number for default setup", type=int)
-    parser.add_argument("-f", "--filepath", type=str, help="filepath to read")
-    parser.add_argument("-o", "--output", type=str, help="file output")
+    # parser.add_argument("-p", "--point", help="point number for default setup", type=int)
+    # parser.add_argument("-f", "--filepath", type=str, help="filepath to read")
+    # parser.add_argument("-o", "--output", type=str, help="file output")
 
     parser.add_argument("--kmeans", action="store_true", help="run k means analysis")
 
