d49d5b663f5f4364f85c51f91aed8fa1d0446fcd
RepleDEV <ragazzochaesa@gmail.com>
2026-05-01 11:25:13 +0700

    update output directory arg parser

--- functions/argparser.py
+++ functions/argparser.py
@@ -9,7 +9,7 @@ def parseArgs():
     parser.add_argument("--ddir", help="set data directory for default setup (defaults to ./data/)", default=DEFAULT_DATA_DIRECTORY)
     # parser.add_argument("-p", "--point", help="point number for default setup", type=int)
     parser.add_argument("-f", "--filepath", type=str, help="filepath to read")
-    # parser.add_argument("-o", "--output", type=str, help="file output")
+    parser.add_argument("--outdir", type=str, help="file output directory")
 
     parser.add_argument("--kmeans", action="store_true", help="run k means analysis")
 
