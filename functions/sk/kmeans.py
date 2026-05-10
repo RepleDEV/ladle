035d12db24db7161c194e0d4b7bd4024a09a1607
RepleDEV <ragazzochaesa@gmail.com>
2026-05-10 14:08:06 +0700

    print clusters in sk.kmeans analysis

--- functions/sk/kmeans.py
+++ functions/sk/kmeans.py
@@ -31,5 +31,20 @@ def run(data: np.ndarray, point_indexes):
     # )
     
     cluster_indexes = kmeans.labels_
+
+    cluster_L_den = [c[0] for c in kmeans.cluster_centers_]
+    cluster_L_den_sort = np.argsort(cluster_L_den)
+
+    print(f"Clusters sorted by L_den: ")
+    for i, c_index in enumerate(cluster_L_den_sort):
+        print_msg = f"{i + 1} (L_den = {cluster_L_den[c_index]}): "
+        points = []
+        for j, point_cluster_index in enumerate(cluster_indexes):
+            if point_cluster_index == c_index:
+                points.append(f"Point {point_indexes[j]}")
+
+        print_msg += ", ".join(points)
+        print(print_msg)
+
     plotClusters(data, cluster_indexes, point_indexes)
     plt.show()
